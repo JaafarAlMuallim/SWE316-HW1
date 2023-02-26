fd0ff6161dcb79ea25f1420c6acebf1ef1ab5f3f
JaafarAlMuallim <Ja3far03@gmail.com>
2023-02-26 20:20:22 +0300

    Added auto full screen on running

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:swe_homework/classes/resume.dart';
 import 'package:swe_homework/misc/read_json.dart';
@@ -5,6 +6,7 @@ import 'package:swe_homework/myapp.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DesktopWindow.toggleFullScreen();
   List<Resume> resumes = await Reader().read();
   runApp(MyApp(
     resumes: resumes,
